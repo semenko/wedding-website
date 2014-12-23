84babafafd550d90411f94977bad064d0afee00e
Nick Semenkovich <semenko@alum.mit.edu>
2014-12-23 13:54:13 -0600

    Styles

--- assets/js/soon/custom.js
+++ assets/js/soon/custom.js
@@ -20,7 +20,7 @@ COMMING SOON PAGE
     function setDate(){
         var now = new Date();
         if( launch < now ){
-            days.html('<h1>0</H1><p>Day</p>');
+            days.html('<h1>0</H1><p class="lede">Day</p>');
             hours.html('<h1>0</h1><p>Hour</p>');
             minutes.html('<h1>0</h1><p>Minute</p>');
             seconds.html('<h1>0</h1><p>Second</p>');
@@ -29,18 +29,18 @@ COMMING SOON PAGE
         else{
             var s = -now.getTimezoneOffset()*60 + (launch.getTime() - now.getTime())/1000;
             var d = Math.floor(s/86400);
-            days.html('<h1>'+d+'</h1><p>Day'+(d>1?'s':''),'</p>');
+            days.html('<h1><b>'+d+'</b></h1><p><b>Day'+(d>1?'s':''),'</b></p>');
             s -= d*86400;
 
             var h = Math.floor(s/3600);
-            hours.html('<h1>'+h+'</h1><p>Hour'+(h>1?'s':''),'</p>');
+            hours.html('<h1><b>'+h+'</b></h1><p><b>Hour'+(h>1?'s':''),'</b></p>');
             s -= h*3600;
 
             var m = Math.floor(s/60);
-            minutes.html('<h1>'+m+'</h1><p>Minute'+(m>1?'s':''),'</p>');
+            minutes.html('<h1><b>'+m+'</b></h1><p><b>Minute'+(m>1?'s':''),'</b></p>');
 
             s = Math.floor(s-m*60);
-            seconds.html('<h1>'+s+'</h1><p>Second'+(s>1?'s':''),'</p>');
+            seconds.html('<h1><b>'+s+'</b></h1><p><b>Second'+(s>1?'s':''),'</b></p>');
             setTimeout(setDate, 1000);
 
             // message.html('OUR SITE IS NOT READY YET, BUT WE ARE COMING SOON');
